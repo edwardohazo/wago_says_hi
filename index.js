4f7e74a9c58e204a7cd123672a363a761c932fae
wago <newguyonthemarket@gmail.com>
2023-08-29 10:05:08 -0700

    Second commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const printMsg = function () {
 	console.log("Hi! Im Wago and this is my first Node Js package");
+	console.log("Incrementing patch to 1.0.1 version");
 }
 
 module.exports = {printMsg};
