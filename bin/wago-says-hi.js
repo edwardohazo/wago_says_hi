324f2f89cbd91b6942d947c534c81d7793137953
wago <newguyonthemarket@gmail.com>
2023-09-02 20:43:42 -0700

    nineth commit

--- bin/wago-says-hi.js
+++ bin/wago-says-hi.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
-const arguments = cli.parse(process.argv);
+
 console.log("Hi! Im Wago and this is my first NPX script");
-console.log("ARGV: ", arguments);
+console.log("ARGV: ", process.argv);
